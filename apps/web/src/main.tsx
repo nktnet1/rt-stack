3ec2de16f457bb385a1871cfd490dbcb8c3e9dd8
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-13 16:57:34 +1100

    feat: updated web app tsconfig

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,4 @@
 import './style.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
@@ -6,4 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
