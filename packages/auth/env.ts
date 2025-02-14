94e9c1f1baabd1c8dff31c1c771d1ec30996a6a9
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-14 15:08:46 +1100

    temp: auth package setup

---
+++ packages/auth/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  runtimeEnv: process.env,
+  clientPrefix: 'VITE_',
+  server: {
+    BETTER_AUTH_URL: z.string().min(1),
+    AUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string().min(1)
+        : z.string().min(1).optional(),
+    NODE_ENV: z.enum(["development", "production"]).optional(),
+  },
+  client: {},
+  skipValidation:
+    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+});
