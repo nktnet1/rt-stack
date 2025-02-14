48127c20f841f36286f9260384343deb760b0d9e
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 01:43:18 +1100

    fix: path with tsconfig and components.json

--- packages/ui/src/dropdown-menu.tsx
+++ packages/ui/src/dropdown-menu.tsx
@@ -8,7 +8,7 @@ import {
 } from '@radix-ui/react-icons';
 import { DropdownMenu as DropdownMenuPrimitive } from 'radix-ui';
 
-import { cn } from '@repo/ui';
+import { cn } from '@/lib';
 
 const DropdownMenu = DropdownMenuPrimitive.Root;
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
