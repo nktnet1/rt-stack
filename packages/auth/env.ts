49c2c90a2de0eff44cf28e5d791b7943dd0c67d4
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-14 16:42:17 +1100

    feat: valibot instead of zod

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -1,16 +1,16 @@
 import { createEnv } from '@t3-oss/env-core';
-import { z } from 'zod';
+import * as v from 'valibot';
 
 export const env = createEnv({
   runtimeEnv: process.env,
   clientPrefix: 'VITE_',
   server: {
-    BETTER_AUTH_URL: z.string().min(1),
+    BETTER_AUTH_URL: v.string(),
     AUTH_SECRET:
       process.env.NODE_ENV === 'production'
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
-    NODE_ENV: z.enum(['development', 'production']).optional(),
+        ? v.pipe(v.string(), v.minLength(1))
+        : v.optional(v.pipe(v.string(), v.minLength(1))),
+    NODE_ENV: v.optional(v.picklist(['development', 'production'])),
   },
   client: {},
   skipValidation:
