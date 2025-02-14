bb6848485680d51405c6d223b62c1c7c32b51fbf
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 02:15:19 +1100

    fix: login page button

--- apps/web/src/routes/login.lazy.tsx
+++ apps/web/src/routes/login.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Button } from '@repo/ui/button';
 
 export const Route = createLazyFileRoute('/login')({
   component: RouteComponent,
@@ -6,8 +7,9 @@ export const Route = createLazyFileRoute('/login')({
 
 function RouteComponent() {
   return (
-    <div className="p-2">
+    <div className="p-2 flex flex-col items-center">
       <h3>Login page</h3>
+      <Button>Submit</Button>
     </div>
   );
 }
