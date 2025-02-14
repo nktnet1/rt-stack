a06416049d11323e1eaff3ecfb6c2b237573453e
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 01:58:10 +1100

    fix: shadcn import

--- packages/ui/src/dropdown-menu.tsx
+++ packages/ui/src/dropdown-menu.tsx
@@ -8,7 +8,7 @@ import {
 } from '@radix-ui/react-icons';
 import { DropdownMenu as DropdownMenuPrimitive } from 'radix-ui';
 
-import { cn } from './';
+import { cn } from '@repo/ui';
 
 const DropdownMenu = DropdownMenuPrimitive.Root;
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
