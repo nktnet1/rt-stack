4e8547f2b06a287ef4a52ff3e827a7a3c3313239
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 01:57:19 +1100

    fix: button

--- packages/ui/src/avatar.tsx
+++ packages/ui/src/avatar.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
 
-import { cn } from '@/lib';
+import { cn } from './';
 
 const Avatar = React.forwardRef<
   React.ComponentRef<typeof AvatarPrimitive.Root>,
