ffea9d09aa193302aa4e88cffd363e0346ab4ec4
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 17:01:30 +1100

    feat: env handlers

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -5,7 +5,13 @@ export const env = createEnv({
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
   server: {
-    BACKEND_PORT: v.number(),
+    BACKEND_PORT: v.pipe(
+      v.string(),
+      v.transform((s) => parseInt(s, 10)),
+      v.number(),
+      v.minValue(1024),
+      v.maxValue(65535),
+    ),
     NODE_ENV: v.optional(v.picklist(['development', 'production'])),
   },
   skipValidation:
