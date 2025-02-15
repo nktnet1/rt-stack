4f46336235706dc203c039414eaf434c4b37685f
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-15 17:10:18 +1100

    fix: better auth url

--- packages/auth/src/env.ts
+++ packages/auth/src/env.ts
@@ -3,18 +3,17 @@ import * as v from 'valibot';
 
 export const env = createEnv({
   runtimeEnv: process.env,
-  clientPrefix: 'VITE_',
   emptyStringAsUndefined: true,
+  clientPrefix: 'VITE_',
+  client: {},
   server: {
-    BETTER_AUTH_URL: v.string(),
-    BACKEND_PORT: v.number(),
+    BETTER_AUTH_URL: v.pipe(v.string(), v.minLength(1)),
     AUTH_SECRET:
       process.env.NODE_ENV === 'production'
         ? v.pipe(v.string(), v.minLength(1))
         : v.optional(v.pipe(v.string(), v.minLength(1))),
     NODE_ENV: v.optional(v.picklist(['development', 'production'])),
   },
-  client: {},
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === 'lint',
 });
