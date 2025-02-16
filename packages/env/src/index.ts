000ff14cb0e610e45c5fba0699ec8e853d9e4cce
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-16 19:13:10 +1100

    style: env index.ts

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -9,15 +9,16 @@ const runtimeEnv =
 export const env = createEnv({
   runtimeEnv,
   emptyStringAsUndefined: true,
+
   // Your vite apps must also define `envPrefix` to be the same.
   clientPrefix: 'PUBLIC_',
   client: {
     PUBLIC_API_URL: v.pipe(v.string(), v.minLength(1), v.url()),
     PUBLIC_WEB_URL: v.pipe(v.string(), v.minLength(1), v.url()),
   },
+
   server: {
     NODE_ENV: v.optional(v.picklist(['development', 'production'])),
-
     API_PORT: v.pipe(
       v.optional(v.string(), '3000'),
       v.transform((s) => parseInt(s, 10)),
@@ -25,11 +26,11 @@ export const env = createEnv({
       v.minValue(1024),
       v.maxValue(65535),
     ),
-
     AUTH_SECRET:
       runtimeEnv.NODE_ENV === 'production'
         ? v.pipe(v.string(), v.minLength(1))
         : v.optional(v.pipe(v.string(), v.minLength(1))),
   },
+
   skipValidation: !!runtimeEnv.CI || runtimeEnv.npm_lifecycle_event === 'lint',
 });
