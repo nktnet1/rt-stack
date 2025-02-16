3ce6fa50dfb50e591b9223ca07b86b761dc44706
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-16 15:02:10 +1100

    feat: auth complete

--- apps/web/src/routes/index.lazy.tsx
+++ apps/web/src/routes/index.lazy.tsx
@@ -1,20 +1,24 @@
 import { createLazyFileRoute, Link } from '@tanstack/react-router';
+import { authClient } from '@repo/auth/client';
 
 export const Route = createLazyFileRoute('/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="p-2">
-      <h3>Hello!</h3>
-      <p className="mt-3">
-        Please{' '}
-        <Link to="/login" className="underline font-bold">
-          log in
-        </Link>{' '}
-        to view your posts.
-      </p>
-    </div>
-  );
+  const { data: session } = authClient.useSession();
+  if (!session) {
+    return (
+      <div className="p-2">
+        <p className="mt-3">
+          Please{' '}
+          <Link to="/login" className="underline font-bold">
+            log in
+          </Link>{' '}
+          to view your posts.
+        </p>
+      </div>
+    );
+  }
+  return <div className="p-2">Welcome, {session.user.name}!</div>;
 }
