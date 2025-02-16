e44092f5152402576d1f134a84d16181d9de6a62
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-16 11:28:34 +1100

    feat: layout component and colours

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from '@/components/layout/navbar';
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
@@ -8,41 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="p-2 flex justify-between text-lg">
-        <div>
-          <Link
-            to="/"
-            activeProps={{
-              className: 'font-bold',
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Home
-          </Link>{' '}
-        </div>
-        <div className="flex gap-1.5">
-          <Link
-            to="/login"
-            activeProps={{
-              className: 'font-bold',
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Login
-          </Link>
-          <span>|</span>
-          <Link
-            to="/register"
-            activeProps={{
-              className: 'font-bold',
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Register
-          </Link>
-        </div>
-      </div>
-      <hr />
+      <Navbar />
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
     </>
