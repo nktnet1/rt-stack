7b61b080d1968527fbd6f192d3c920043eb6d96c
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-18 10:36:00 +1100

    fix: add margin to form field info

--- apps/web/src/components/forms/field-info.tsx
+++ apps/web/src/components/forms/field-info.tsx
@@ -6,13 +6,11 @@ export default function FormFieldInfo({
   field: FieldApi<any, any, any, any>;
 }) {
   return (
-    <>
+    <div className="mt-2">
       {field.state.meta.isTouched && field.state.meta.errors.length ? (
-        <em className="text-destructive">
-          {field.state.meta.errors.join(',')}
-        </em>
+        <em className="text-red-500">{field.state.meta.errors.join(',')}</em>
       ) : null}
       {field.state.meta.isValidating ? 'Validating...' : null}
-    </>
+    </div>
   );
 }
