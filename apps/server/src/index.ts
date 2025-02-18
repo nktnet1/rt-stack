6c93e34b2a94d10389187fccbec58d47bd832a65
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-18 13:26:35 +1100

    feat(server): hono logger

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,7 +4,8 @@ import { auth } from '@repo/auth/server';
 import { env } from '@repo/env';
 import { cors } from 'hono/cors';
 import { trpcServer } from '@hono/trpc-server';
-import { appRouter } from '@repo/api/server';
+import { appRouter, createTRPCContext } from '@repo/api/server';
+import { logger } from 'hono/logger';
 
 const app = new Hono<{
   Variables: {
@@ -12,6 +13,8 @@ const app = new Hono<{
     session: typeof auth.$Infer.Session.session | null;
   };
 }>();
+
+app.use(logger());
 
 app.use(
   '/api/auth/*',
@@ -27,24 +30,18 @@ app.use(
 
 app.use('*', async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
-
-  if (!session) {
-    c.set('user', null);
-    c.set('session', null);
-    return next();
-  }
-
-  c.set('user', session.user);
-  c.set('session', session.session);
+  c.set('user', session?.user ?? null);
+  c.set('session', session?.session ?? null);
   return next();
 });
 
-app.on(['POST', 'GET'], '/api/auth/**', (c) => auth.handler(c.req.raw));
+app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
 
 app.use(
   '/trpc/*',
   trpcServer({
     router: appRouter,
+    createContext: (c) => createTRPCContext({ headers: c.req.headers }),
   }),
 );
 
@@ -57,4 +54,4 @@ serve({
   port: env.API_PORT,
 });
 
-console.log(`Server is running on http://localhost:${env.API_PORT}`);
+console.log(`Hono server: http://localhost:${env.API_PORT}`);
