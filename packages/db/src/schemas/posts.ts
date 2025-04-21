32f56584fa41c38b0b39e09686c789946fb161ff
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-04-21 21:55:44 +1000

    refactor(db): order of null constraint and defaults for post createdAt

--- packages/db/src/schemas/posts.ts
+++ packages/db/src/schemas/posts.ts
@@ -7,7 +7,7 @@ export const post = pgTable('post', (t) => ({
   id: t.uuid().primaryKey().defaultRandom(),
   title: t.varchar({ length: 256 }).notNull(),
   content: t.text().notNull(),
-  createdAt: t.timestamp().defaultNow().notNull(),
+  createdAt: t.timestamp().notNull().defaultNow(),
   createdBy: t
     .text()
     .references(() => user.id)
