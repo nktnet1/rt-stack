17a01f6382a9462b254a416c95d73f9228be12d4
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-08-30 11:47:54 +1000

    fix(db): createAt timestamp with timezone

--- packages/db/src/schemas/posts.ts
+++ packages/db/src/schemas/posts.ts
@@ -7,7 +7,10 @@ export const post = pgTable('post', (t) => ({
   id: t.uuid().primaryKey().defaultRandom(),
   title: t.varchar({ length: 256 }).notNull(),
   content: t.text().notNull(),
-  createdAt: t.timestamp({ mode: 'string' }).notNull().defaultNow(),
+  createdAt: t
+    .timestamp({ mode: 'string', withTimezone: true })
+    .notNull()
+    .defaultNow(),
   createdBy: t
     .text()
     .references(() => user.id)
