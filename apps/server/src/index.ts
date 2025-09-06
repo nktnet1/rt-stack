17e37bdd5eefb782760a9be4681ff29ab003c6fa
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-09-07 09:33:14 +1000

    refactor(server): export app in index.ts, create new server.ts entrypoint

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,3 @@
-import { serve } from '@hono/node-server';
 import { createApi } from '@repo/api/server';
 import { createAuth } from '@repo/auth/server';
 import { createDb } from '@repo/db/client';
@@ -84,33 +83,4 @@ app.use(
 
 // ========================================================================= //
 
-const server = serve(
-  {
-    fetch: app.fetch,
-    port: env.SERVER_PORT,
-    hostname: env.SERVER_HOST,
-  },
-  (info) => {
-    const host = info.family === 'IPv6' ? `[${info.address}]` : info.address;
-    console.log(`
-Hono
-- internal server url: http://${host}:${info.port}
-- external server url: ${env.PUBLIC_SERVER_URL}
-- public web url: ${env.PUBLIC_WEB_URL}
-    `);
-  },
-);
-
-const shutdown = () => {
-  server.close((error) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('\nServer has stopped gracefully.');
-    }
-    process.exit(0);
-  });
-};
-
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+export default app;
