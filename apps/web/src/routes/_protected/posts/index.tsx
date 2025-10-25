7d1ff6e1849875a5d3be536e2d1449cf64239683
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-25 22:53:18 +1100

    fix(web): remove border on posts heading

--- apps/web/src/routes/_protected/posts/index.tsx
+++ apps/web/src/routes/_protected/posts/index.tsx
@@ -104,7 +104,7 @@ function RouteComponent() {
     );
   return (
     <div className="flex flex-col p-1.5 md:p-4 w-full max-w-6xl mx-auto">
-      <div className="flex items-center justify-between border">
+      <div className="flex items-center justify-between">
         <h1 className="text-2xl">Posts</h1>
         <CreatePostButton />
       </div>
