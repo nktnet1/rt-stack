111f0eba9b646fb8bb215d99f985aba00e6c082d
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-03-23 08:52:31 +1100

    fix(web): apply ReturnType<typeof createAuthClient> workaround
    Ref:
    - https://github.com/better-auth/better-auth/issues/8623#issuecomment-4064187625

--- apps/web/src/clients/authClient.ts
+++ apps/web/src/clients/authClient.ts
@@ -1,11 +1,11 @@
 import { createAuthClient } from '@repo/auth/client';
 import { env } from '@/env';
 
+type AuthClient = ReturnType<typeof createAuthClient>;
+
 export const authClient = createAuthClient({
   apiBaseUrl: env.PUBLIC_SERVER_URL,
   apiBasePath: env.PUBLIC_SERVER_API_PATH,
-});
+}) as AuthClient;
 
-export type AuthSession =
-  | ReturnType<typeof createAuthClient>['$Infer']['Session']
-  | null;
+export type AuthSession = AuthClient['$Infer']['Session'] | null;
